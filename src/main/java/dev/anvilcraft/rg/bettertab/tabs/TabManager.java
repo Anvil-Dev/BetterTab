018ad177da399472be3386da88d15cba6c24dec9
Gugle <administration@ztxy666.cn>
2025-02-07 13:55:13 +0800

    将 TabManager 改为 LinkedHashMap 以保持插入顺序

--- src/main/java/dev/anvilcraft/rg/bettertab/tabs/TabManager.java
+++ src/main/java/dev/anvilcraft/rg/bettertab/tabs/TabManager.java
@@ -7,31 +7,31 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public class TabManager {
-    public static final Map<Supplier<Boolean>, Tab> tabs = new HashMap<>();
+    public static final Map<Supplier<Boolean>, Tab> TABS = new LinkedHashMap<>();
 
     static {
-        tabs.put(() -> BetterTabServerRules.showTps, new TpsTab());
-        tabs.put(() -> BetterTabServerRules.showMobcaps, new MobcapsTab());
-        tabs.put(
+        TABS.put(() -> BetterTabServerRules.showTps, new TpsTab());
+        TABS.put(() -> BetterTabServerRules.showMobcaps, new MobcapsTab());
+        TABS.put(
             () -> !BetterTabServerRules.customTabLine.equals("none"),
             (s, p) -> Component.literal(BetterTabServerRules.customTabLine.replace("&&", "§"))
         );
     }
 
     public static boolean shouldShow() {
-        for (Supplier<Boolean> supplier : tabs.keySet()) if (supplier.get()) return true;
+        for (Supplier<Boolean> supplier : TABS.keySet()) if (supplier.get()) return true;
         return false;
     }
 
     public static @NotNull Component getTabs(MinecraftServer server, ServerPlayer player) {
         MutableComponent component = Component.literal("");
         int length = 0;
-        for (Map.Entry<Supplier<Boolean>, Tab> entry : tabs.entrySet()) {
+        for (Map.Entry<Supplier<Boolean>, Tab> entry : TABS.entrySet()) {
             if (!entry.getKey().get()) continue;
             if (length != 0) component.append("\n");
             length++;
